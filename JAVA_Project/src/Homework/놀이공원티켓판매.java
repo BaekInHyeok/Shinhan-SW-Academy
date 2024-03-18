7cc4703b6b2543c94785a2bc08f15ef486f6e637
BaekInHyeok <bih63879@gmail.com>
2024-03-18 09:51:58 +0900

    동 우선순위 내 티켓 구매량에 따른 우선순위 구현

--- JAVA_Project/src/Homework/놀이공원티켓판매.java
+++ JAVA_Project/src/Homework/놀이공원티켓판매.java
@@ -1,6 +1,8 @@
 package Homework;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class 놀이공원티켓판매 {
 
@@ -26,17 +28,26 @@ public class 놀이공원티켓판매 {
 	// requests: 회원 등급, 티켓 구매 수량
 	public static int solution(int tickets, int[][] requests) {
         //이부분 구현 
+		//등급 우선순위는 priority 변수를 이용한 for문으로 결정
+		//같은 등급 내 우선순위는 Arraylist를 이용하여 결정
 		int priority=1;
 		int totalSell=0;
-		
+		ArrayList<Integer> reqArr = new ArrayList<Integer>();
 		while(priority<=10){
 			for(int i=0;i<requests.length;i++) {
 				if(requests[i][0]==priority) {
-					if(requests[i][1]<=tickets) {
-						tickets=tickets-requests[i][1];
-						totalSell+=requests[i][1];
+					reqArr.add(requests[i][1]);
+				}
+				
+				reqArr.sort(Comparator.reverseOrder());
+				
+				for(int j=0;j<reqArr.size();j++) {
+					if(reqArr.get(j)<=tickets) {
+						tickets=tickets-reqArr.get(j);
+						totalSell+=reqArr.get(j);
 					}
 				}
+				reqArr.clear();
 			}
 			priority++;
 		}
